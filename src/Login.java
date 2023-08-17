5af25db873a6ebc7c1f60fe2993a6c8e2c97d1c1
cristian-simba <davidsimba200@gmail.com>
2023-08-16 19:22:36 -0500

    avanceLogin

--- src/Login.java
+++ src/Login.java
@@ -1,2 +1,22 @@
+import javax.swing.*;
+
 public class Login {
+
+    private JPanel Login;
+    private JComboBox rolBox;
+    private JTextField user;
+    private JPasswordField password;
+    private JPanel Imagen;
+    private JPanel Datos;
+    private JButton entrarButton;
+
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Login");
+        frame.setContentPane(new Login().Login);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 350);
+        frame.setVisible(true);
+    }
 }
+
