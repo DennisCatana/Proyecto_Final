691022f7dccbf357efb7f2a461f0c2c09139ea32
cristian-simba <davidsimba200@gmail.com>
2023-08-19 21:06:16 -0500

    Maqueta Menu Administrador

--- src/AdminMenu.java
+++ src/AdminMenu.java
@@ -2,13 +2,19 @@ import javax.swing.*;
 
 public class AdminMenu {
     private JPanel AdminMenu;
+    private JPanel Opciones;
+    private JButton ventasButton;
+    private JButton cajerosButton;
+    private JButton cerrarSesiónButton;
+    private JButton productosButton;
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Menu Principal");
+        JFrame frame = new JFrame("Administrador - Menu Principal");
         frame.setContentPane(new AdminMenu().AdminMenu);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(900, 550);
+        frame.setSize(1000, 450);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+
     }
 }
