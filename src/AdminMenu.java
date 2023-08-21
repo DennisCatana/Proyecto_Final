0c91dd88ea6bc8d45434e46d5f03b1cf28a3aa30
cristian-simba <davidsimba200@gmail.com>
2023-08-21 15:39:37 -0500

    Imgs Menú Administrados

--- src/AdminMenu.java
+++ src/AdminMenu.java
@@ -4,9 +4,10 @@ public class AdminMenu {
     private JPanel AdminMenu;
     private JPanel Opciones;
     private JButton ventasButton;
-    private JButton cajerosButton;
     private JButton cerrarBt;
     private JButton productosButton;
+    private JButton cajerosButton;
+    private JPanel Productos;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("Administrador - Menu Principal");
