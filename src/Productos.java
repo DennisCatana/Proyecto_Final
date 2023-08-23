b09a574e997131198103aee76b461cb81a97fce3
iveth-cocha <maria.iveth16@outlook.es>
2023-08-22 23:53:13 -0500

    boton regresar-productos

--- src/Productos.java
+++ src/Productos.java
@@ -1,7 +1,21 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Productos {
      JPanel Productos;
+    private JButton regresarButton;
+
+
+    public Productos() {
+        regresarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
     private void closeProductosFrame() {
         JFrame loginFrame = (JFrame) SwingUtilities.getWindowAncestor(Productos);
         loginFrame.dispose();}
