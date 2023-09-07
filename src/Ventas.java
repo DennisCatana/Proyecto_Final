fd0d830f52173a51aac7051a4992b4e1b879fa70
iveth-cocha <maria.iveth16@outlook.es>
2023-09-07 18:05:42 -0500

    boton ventas

--- src/Ventas.java
+++ src/Ventas.java
@@ -13,7 +13,7 @@ public class Ventas {
     private JButton ventasButton;
     static final String DB_URL="jdbc:mysql://localhost/Medical";
     static final String USER="root";
-    static final String PASS="root";
+    static final String PASS="poo123";
     static final String QUERY="Select * From NotaDeVenta";
 
     public Ventas() {
@@ -83,13 +83,47 @@ public class Ventas {
         ventasButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JFrame frame = new JFrame("Ventas");
-                frame.setContentPane(new Ventas().Ventas);
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                closeVentasFrame();
-                frame.setSize(1000, 450);
-                frame.setLocationRelativeTo(null);
-                frame.setVisible(true);
+                DefaultTableModel model = new DefaultTableModel();
+                model.addColumn("idUsuario");
+                model.addColumn("idNotaVenta");
+                model.addColumn("fecha");
+                model.addColumn("subtotal");
+                model.addColumn("iva");
+                model.addColumn("Total");
+                model.addColumn("nombreCliente");
+                model.addColumn("cedulaCliente");
+                model.addColumn("direccionCliente");
+
+                // Poner las columnas en el modelo hecho en el Jtable
+                ventas.setModel(model);
+
+                //arreglo que almnacena registro de ventas
+                String [] infoVentas=new String[9];//especifico el numero de columnas
+
+                try{
+                    Connection conn = DriverManager.getConnection(DB_URL,USER,PASS);
+                    Statement stmt= conn.createStatement();
+                    ResultSet rs= stmt.executeQuery(QUERY);
+
+                    while (rs.next()){
+                        //detallo la posicion de dato almacenado en arreglo, con la columna en la quebe ir
+                        infoVentas[0]=rs.getString(6);//num de columna
+                        infoVentas[1]=rs.getString(1);
+                        infoVentas[2]=rs.getString(2);
+                        infoVentas[3]=rs.getString(4);
+                        infoVentas[4]=rs.getString(5);
+                        infoVentas[5]=rs.getString(3);
+                        infoVentas[6]=rs.getString(7);
+                        infoVentas[7]=rs.getString(8);
+                        infoVentas[8]=rs.getString(9);
+
+                        // genera una fila por cada ingistro
+                        model.addRow(infoVentas);
+                    }
+                } catch (SQLException m) {
+                    //throw new RuntimeException(e);
+                    JOptionPane.showMessageDialog(null,"Error"+m.toString());
+                }
             }
         });
     }
